93adeb2139e2530b933ed76cadb7a771a436d6c2
qschweitzer <36896812+qschweitzer@users.noreply.github.com>
2018-08-13 22:59:27 +0200

    Major changes

--- RESTapi.ps1
+++ RESTapi.ps1
@@ -25,157 +25,243 @@ CODES STATUS
 500 Server error Le serveur a rencontré un problème.
 
 #>
-Function Invoke-PRCommand {
-param(
-    [parameter(Mandatory=$true)]
-    $RequestType,
-    [parameter(Mandatory=$true)]
-    $URL
-)
-    $Noresult = "There is something wrong with your query or the result."
-    switch($RequestType)
-    {
-        "POST" {
-            try{
-                # Build command
-                $Request = $URL.split("/")[3]
-                
-                if($Request -like "Create-*" -OR $Request -like "New-*" -OR $Request -like "Invoke-*"){
-                    # Invoke command on the local computer
-                    $Result = Invoke-Expression -Command $Request -ErrorAction Stop
-                }
-                else{
-                    $Result = "Error: Not a Create or New or Invoke verb invoked"
-                }
-            }catch{
-                # Build response
-                $Result = ("Error: " + $_.Exception.Message)
-            }
-        }
-        "GET" {
-            try{
-                # Build command
-                write-host $URL
-                $Request = $URL.split("/")[3]
-                
-                if($Request -like "Get-*"){
-                    # Invoke command on the local computer
-                    $Result = Invoke-Expression -Command $Request -ErrorAction Stop
-                }
-                else{
-                    $Result = "Error: Not a Get verb invoked"
-                }
-            }catch{
-                # Build response
-                $Result = ("Error: " + $_.Exception.Message)
-            }
-        }
-        "PUT" {
-            try{
-                # Build command
-                $Request = $URL.split("/")[3]
-                
-                if($Request -like "Set-*"){
-                    # Invoke command on the local computer
-                    $Result = Invoke-Expression -Command $Request -ErrorAction Stop
-                }
-                else{
-                    $Result = "Error: Not a Set verb invoked"
-                }
-            }catch{
-                # Build response
-                $Result = ("Error: " + $_.Exception.Message)
-            }
-        }
-        "DELETE" {
-            try{
-                # Build command
-                $Request = $URL.split("/")[3]
-                
-                if($Request -like "Remove-*"){
-                    # Invoke command on the local computer
-                    $Result = Invoke-Expression -Command $Request -ErrorAction Stop
-                }
-                else{
-                    $Result = "Error: Not a Remove verb invoked"
-                }
-            }catch{
-                # Build response
-                $Result = ("Error: " + $_.Exception.Message)
-            }
-        }
-    }
-    return $Result
-}
-
 Function Start-PRListener {
-
     param(
-        [parameter(Mandatory=$true)]
-        $ListingPort
+        [parameter(Mandatory = $true)]
+        $ScriptBlock,
+        $ListeningPort
     )
     # Create a listener on port specified
-    $listener = New-Object System.Net.HttpListener
-    $listener.Prefixes.Add("http://localhost:$ListingPort/") 
-    $listener.Start()
-    write-host "Listening on port $ListingPort..."
-    write-host "API Access: http://localhost:$ListingPort/"
-    write-host "To Stop API Listner: http://localhost:$ListingPort/kill"
+    $URL = "http://localhost:$ListeningPort/"
+    # Create HttpListener Object
+    write-host "Listening on port $ListeningPort..."
+    $SimpleServer = New-Object Net.HttpListener
+    write-host "To Stop API Listner: $($URL)kill"
 
-    # Run until you send a GET request to /end
-    while ($true) {
+    # Tell the HttpListener what port to listen on
+    #    As long as we use localhost we don't need admin rights. To listen on externally accessible IP addresses we will need admin rights
+    write-host "API Access: $URL"
+    $SimpleServer.Prefixes.Add($URL)
 
-        $context = $listener.GetContext() 
+    # Start up the server
+    $SimpleServer.Start()
 
-        # Capture the details about the request
-        $request = $context.Request
+    while($SimpleServer.IsListening)
+    {
+        Write-Host "Listening for request"
+        # Tell the server to wait for a request to come in on that port.
+        $Context = $SimpleServer.GetContext()
 
-        # Setup a place to deliver a response
-        $IPR_Response = $context.Response
-    
-        # Break from loop if GET request sent to /end
-        if ($request.Url -match '/kill') { 
-            break 
-        }else{
+        #Once a request has been captured the details of the request and the template for the response are created in our $context variable
+        Write-Verbose "Context has been captured"
 
-            # Split request URL to get command and options
-            $requestvars = ([String]$request.Url).split("/")
-            if($requestvars.Count -le "3"){
-                $IPR_Return = "Your request is not correct. Please look at the help of the API."
-                $IPR_Response.ContentType = 'text/html'
-                break
+        # $Context.Request contains details about the request
+        # $Context.Response is basically a template of what can be sent back to the browser
+        # $Context.User contains information about the user who sent the request. This is useful in situations where authentication is necessary
+
+
+        # Sometimes the browser will request the favicon.ico which we don't care about. We just drop that request and go to the next one.
+        if($Context.Request.Url.LocalPath -eq "/favicon.ico")
+        {
+            do
+            {
+
+                    $Context.Response.Close()
+                    $Context = $SimpleServer.GetContext()
+
+            }while($Context.Request.Url.LocalPath -eq "/favicon.ico")
+        }
+
+        [string]$URLActive = $Context.Request.Url
+
+        # Creating a friendly way to shutdown the server
+        if($Context.Request.Url.LocalPath -eq "/kill")
+        {
+
+                    $Context.Response.Close()
+                    $SimpleServer.Stop()
+                    break
+
+        }
+        elseif($URLActive.split("/").count -le 3){
+            $result = "Error: No parameters after base URL. Please specify which computer you want to connect to and what action have to be done."
+        }
+        else{
+            $result = try {
+                .$ScriptBlock
+            } catch {
+                $_.Exception.Message
             }
-            else{
-                # Start the function Invoke-PRCommand with parameters
-                $IPR_Return = Invoke-PRCommand -RequestType $context.Request.HttpMethod -URL ([String]$request.Url)
-                $IPR_Return.gettype()
-                if($IPR_Return -like "Error:*"){
-                    $IPR_Response.ContentType = 'text/html'
-                }else{
-                    # Convert the returned data to JSON and set the HTTP content type to JSON
-                    $IPR_Response.ContentType = 'application/json'
+        }
+    
+        $Context.request
+
+        if($result -ne $null) {
+            if($result -is [string]){
+                
+                Write-Verbose "A [string] object was returned. Writing it directly to the response stream."
+
+            } else {
+
+                Write-Verbose "Converting PS Objects into JSON objects"
+                $result = $result | ConvertTo-Json
+                
+            }
+        }
+
+        Write-Host "Sending response of $Result"
+
+        # We convert the result to bytes from ASCII encoded text
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes($Result)
+
+        # We need to let the browser know how many bytes we are going to be sending
+        $context.Response.ContentLength64 = $buffer.Length
+
+        # We send the response back to the browser
+        $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
+
+        # We close the response to let the browser know we are done sending the response
+        $Context.Response.Close()
+
+        $Context.Response
+    }
+}
+Function Invoke-PRCommand {
+
+    $Noresult = "There is something wrong with your query or the result."
+    # Listenning port
+    Do {
+        [int]$PRPort = Read-Host -Prompt "Which port should I use ?"
+    }Until($PRPort -ne $null)
+
+    Start-PRListener -ListeningPort $PRPort -ScriptBlock {
+
+        [string]$URL = ($Context.Request.URL)
+
+        switch ($Context.Request.HttpMethod) {
+            "POST" {
+                try {
+                    # Build command
+                    #write-host ("URLfull= " + $URL)
+                    [string]$Verb = $URL.split("/")[3]
+                    
+                    if ($Verb -eq "CREATE" -OR $Verb -eq "NEW" -OR $Verb -eq "INVOKE") {
+                        #write-host ("Verb= " + $Verb)
+                        [string]$What2Do = $URL.split("/")[4]
+                        #write-host ("What2Do= " + $What2Do)
+                        [string]$Arguments = ""
+                    
+                        if ($URL.split("/").count -gt 5) {
+                            $Arguments = ($URL.split("/")[5]).replace("&","-").replace("="," ")
+                        }
+                        
+                        # Invoke command on the local computer
+                        [string]$Command = "$Verb-$What2Do $Arguments"
+                        Write-Host ("Command= " + $command )
+                        $Result = Invoke-Expression $Command -ErrorAction Stop
+                    }
+                    else {
+                        $Result = "Error: Not a Create or New or Invoke verb invoked"
+                    }
+                }
+                catch {
+                    # Build response
+                    $Result = ("Error: " + $_.Exception.Message)
                 }
             }
-                # Convert the data to UTF8 bytes
-                [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($IPR_Return)
-                
-                # Set length of response
-                $IPR_Response.ContentLength64 = $buffer.length
-                
-                # Write response out and close
-                $output = $IPR_Response.OutputStream
-                $output.Write($buffer, 0, $buffer.length)
-                $output.Close()
+            "GET" {
+                try {
+                    # Build command
+                    #write-host ("URLfull= " + $URL)
+                    [string]$Verb = $URL.split("/")[3]
+
+                    if ($Verb -eq "GET"){
+                        #write-host ("Verb= " + $Verb)
+                        [string]$What2Do = $URL.split("/")[4]
+                        #write-host ("What2Do= " + $What2Do)
+                        [string]$Arguments = ""
+                    
+                        if ($URL.split("/").count -gt 5) {
+                            $Arguments = ($URL.split("/")[5]).replace("&","-").replace("="," ")
+                        }
+                        
+                        # Invoke command on the local computer
+                        [string]$Command = "$Verb-$What2Do $Arguments"
+                        Write-Host ("Command= " + $command )
+                        $Result = Invoke-Expression $Command -ErrorAction Stop
+                    }
+                    else {
+                        $Result = "Error: Not a Create or New or Invoke verb invoked"
+                    }
+                }
+                catch {
+                    # Build response
+                    $Result = ("Error: " + $_.Exception.Message + " " + $error)
+                }
+            }
+            "PUT" {
+                try {
+                    # Build command
+                    #write-host ("URLfull= " + $URL)
+                    [string]$Verb = $URL.split("/")[3]
+                    
+                    if ($Verb -eq "SET") {
+                        #write-host ("Verb= " + $Verb)
+                        [string]$What2Do = $URL.split("/")[4]
+                        #write-host ("What2Do= " + $What2Do)
+                        [string]$Arguments = ""
+                    
+                        if ($URL.split("/").count -gt 5) {
+                            $Arguments = ($URL.split("/")[5]).replace("&","-").replace("="," ")
+                        }
+                        
+                        # Invoke command on the local computer
+                        [string]$Command = "$Verb-$What2Do $Arguments"
+                        Write-Host ("Command= " + $command )
+                        $Result = Invoke-Expression $Command -ErrorAction Stop
+                    }
+                    else {
+                        $Result = "Error: Not a Create or New or Invoke verb invoked"
+                    }
+                }
+                catch {
+                    # Build response
+                    $Result = ("Error: " + $_.Exception.Message)
+                }
+            }
+            "DELETE" {
+                try {
+                    # Build command
+                    #write-host ("URLfull= " + $URL)
+                    [string]$Verb = $URL.split("/")[3]
+                    
+                    if ($Verb -eq "REMOVE") {
+                        #write-host ("Verb= " + $Verb)
+                        [string]$What2Do = $URL.split("/")[4]
+                        #write-host ("What2Do= " + $What2Do)
+                        [string]$Arguments = ""
+                    
+                        if ($URL.split("/").count -gt 5) {
+                            $Arguments = ($URL.split("/")[5]).replace("&","-").replace("="," ")
+                        }
+                        
+                        # Invoke command on the local computer
+                        [string]$Command = "$Verb-$What2Do $Arguments"
+                        Write-Host ("Command= " + $command )
+                        $Result = Invoke-Expression $Command -ErrorAction Stop
+                    }
+                    else {
+                        $Result = "Error: Not a Create or New or Invoke verb invoked"
+                    }
+                }
+                catch {
+                    # Build response
+                    $Result = ("Error: " + $_.Exception.Message)
+                }
+            }
         }
+        return $Result
     }
-    
-    #Terminate the listener
-    $listener.Stop()
 }
 
-# Listenning port
-Do {
-    [int]$PRPort = Read-Host -Prompt "Which port should I use ?"
-}Until($PRPort -ne $null)
-
-Start-PRListener -ListingPort $PRPort
+Invoke-PRCommand
